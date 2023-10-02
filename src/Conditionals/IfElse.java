99279a2d8d7973c9b69e85c7983a2f50cfbe2dc7
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-02 18:46:31 +0300

    Conditionals

---
+++ src/Conditionals/IfElse.java
@@ -0,0 +1,27 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class IfElse {
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+
+            System.out.println("Enter you name");
+            String name = s.next();
+
+            System.out.println("Your age");
+            int age = s.nextInt();
+
+            if (age > 18){
+                System.out.println("You can visit this system.");
+            } else if (age < 18) {
+                System.out.println("Sorry, you can not visit the system.");
+                int whenCan = 18-age;
+                System.out.println("You can try after "+whenCan+" years");
+            }else{
+                System.out.println("First year how you can visit the system.");
+            }
+
+        }
+    }
+}
---
+++ src/Conditionals/Switch.java
@@ -0,0 +1,44 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        int day;
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please, enter number from 1 to 7");
+
+        day = s.nextInt();
+
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+        if (day>7){
+            System.out.println("Week has 7 days only");
+        }else {
+            System.out.println("Do you know that week has more days?");
+        }
+    }
+}
+
